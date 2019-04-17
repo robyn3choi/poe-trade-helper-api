12516387a441dde287fb43d16d80cfd789f6ac3d
unknown <stuart.brookes@outlook.com>
2019-04-16 21:05:14 -0700

    built item list and got prices for items with new api

--- items.js
+++ items.js
@@ -1,281 +1,283 @@
-let items = [
+let cardIdToIndex = {};
+
+let itemCardTableEntries = [
   {
-    id: 3283,
+    itemId: 3283,
     name: 'Mirror of Kalandra',
     cardId: 3776,
     card: "House of Mirrors"
   },
   {
-    id: 3891,
+    itemId: 3891,
     name: 'Headhunter',
     cardId: 3509,
     card: "The Doctor"
   },
   {
-    id: 3891,
+    itemId: 3891,
     name: 'Headhunter',
     cardId: 3423,
     card: "The Fiend"
   },
   {
-    id: 3841,
+    itemId: 3841,
     name: "Queen's Sacrifice",
     cardId: 3616,
     card: "Beauty Through Death"
   },
   {
-    id: 5667,
+    itemId: 5667,
     name: "Trash To Treasure",
     cardId: 374,
     card: "The Iron Bard"
   },
   {
-    id: 6371,
+    itemId: 6371,
     name: "Zerphi's Heart",
     cardId: 12642,
     card: "The Life Thief"
   },
   {
-    id: 3776,
+    itemId: 3776,
     name: "House Of Mirrors",
     cardId: 3604,
     card: "The Immortal"
   },
   {
-    id: 3309,
+    itemId: 3309,
     name: "The Perandus Manor",
     cardId: 2863,
     card: "The Mayor"
   },
   {
-    id: 3777,
+    itemId: 3777,
     name: "Fated Connections",
     cardId: 3144,
     card: "Immortal Resolve"
   },
   {
-    id: 2754,
+    itemId: 2754,
     name: "Berek's Respite",
     cardId: 493,
     card: "The Spark And The Flame"
   },
   {
-    id: 3509,
+    itemId: 3509,
     name: "The Doctor",
     cardId: 3088,
     card: "The Nurse"
   },
   {
-    id: 8,
+    itemId: 8,
     name: "Watcher's Eye",
     cardId: 544,
     card: "The Samurai's Eye"
   },
   {
-    id: -1,       // create custom rule for this ID number
+    itemId: -1,       // create custom rule for this ID number
     name: "3x Exalted Orb",
     cardId: 2331,
     card: "Abandoned Wealth"
   },
   {
-    id: 3840,
+    itemId: 3840,
     name: "The Taming",
     cardId: 2272,
     card: "Hunter's Reward"
   },
   {
-    id: 5776,
+    itemId: 5776,
     name: "Opal Ring ilvl 100",
     cardId: 2355,
     card: "The Celestial Stone"
   },
   {
-    id: 5498,
+    itemId: 5498,
     name: "Mirror Shard",
     cardId: 12507,
     card: "Seven Years Bad Luck"
   },
   {
-    id: 3351,
+    itemId: 3351,
     name: "Lvl 4 Enlighten",
     cardId: 4207,
     card: "Wealth And Power"
   },
   {
-    id: 3519,
+    itemId: 3519,
     name: "Lvl 4 Empower",
     cardId: 2616,
     card: "The Dragon's Heart"
   },
   {
-    id: 3783,
+    itemId: 3783,
     name: "Rigwald's Quills",
     cardId: 2927,
     card: "The Wolven King's Bite"
   },
   {
-    id: 251, //maybe remove this one cuz of weird prices
+    itemId: 251, //maybe remove this one cuz of weird prices
     name: "Ventor's Gamble",
     cardId: 2627,
     card: "The Risk"
   },
   {
-    id: -2,
+    itemId: -2,
     name: "2x Exalted Orb",
     cardId: 1441,
     card: "The Saint's Treasure"
   },
   {
-    id: 2644,
+    itemId: 2644,
     name: "Eyes Of The Greatwolf",
     cardId: 3424,
     card: "Mawr Blaidd"
   },
   {
-    id: 8836,
+    itemId: 8836,
     name: "Atziri's Acuity",
     cardId: 4270,
     card: "The Queen"
   },
   {
-    id: 2644,
+    itemId: 2644,
     name: "Kaom's Heart (Corrupted)",
     cardId: 2506,
     card: "Pride Before The Fall"
   },
   {
-    id: 4189,
+    itemId: 4189,
     name: "Lvl 4 Enhance",
     cardId: 3095,
     card: "The Artist"
   },
   {
-    id: 2644,
+    itemId: 2644,
     name: "Kaom's Heart",
     cardId: 1316,
     card: "The King's Heart"
   },
   {
-    id: 2497,
+    itemId: 2497,
     name: "The Putrid Cloister",
     cardId: 2375,
     card: "The Professor"
   },
   {
-    id: -3,
+    itemId: -3,
     name: "3x Orb Of Annulment",
     cardId: 12568,
     card: "The Seeker"
   },
   // Can't look up values of these items
   // {
-  //   id: null,
+  //   itemId: null,
   //   name: "6L Astral Plate",
   //   cardId: 2516,
   //   card: "The Celestial Justicar"
   // },
   // {
-  //   id: null,
+  //   itemId: null,
   //   name: "6L Vaal Regalia",
   //   cardId: 1901,
   //   card: "The Ethereal"
   // },
   {
-    id: 2504,
+    itemId: 2504,
     name: "Lvl 3 Enlighten",
     cardId: 645,
     card: "The Enlightened"
   },
   {
-    id: 142,
+    itemId: 142,
     name: "Exalted Orb",
     cardId: 811,
     card: "The Hoarder"
   },
   {
-    id: 3428,
+    itemId: 3428,
     name: "The Beachhead",
     cardId: 12519,
     card: "The Landing"
   },
   {
-    id: 2251,
+    itemId: 2251,
     name: "Shavronne's Wrappings",
     cardId: 788,
     card: "The Offering"
   },
   {
-    id: 3301,
+    itemId: 3301,
     name: "Voll's Devotion (Corrupted)",
     cardId: 2626,
     card: "The Brittle Emperor"
   },
   {
-    id: 1165,
+    itemId: 1165,
     name: "Bisco's Collar",
     cardId: 1610,
     card: "The Master"
   },
   {
-    id: 1279,
+    itemId: 1279,
     name: "Mortal Hope",
     cardId: 1963,
     card: "Last Hope"
   },
   {
-    id: 1467,
+    itemId: 1467,
     name: "Astramentis",
     cardId: 875,
     card: "The Polymath"
   },
   {
-    id: 1336,
+    itemId: 1336,
     name: "Monstrous Treasure",
     cardId: 674,
     card: "The Valley Of Steel Boxes"
   },
   {
-    id: 1352,
+    itemId: 1352,
     name: "Windripper",
     cardId: 524,
     card: "The Wind"
   },
   {
-    id: 1036,
+    itemId: 1036,
     name: "Kaom's Roots",
     cardId: 1569,
     card: "The Throne"
   },
   {
-    id: 144,
+    itemId: 144,
     name: "Taste Of Hate",
     cardId: 1266,
     card: "The Hunger"
   },
   {
-    id: 794,
+    itemId: 794,
     name: "The Jeweller's Touch",
     cardId: 1344,
     card: "The Jeweller's Boon"
   },
   {
-    id: 1436,
+    itemId: 1436,
     name: "Starforge",
     cardId: 1184,
     card: "The World Eater"
   },
   {
-    id: 907,
+    itemId: 907,
     name: "The Poet's Pen",
     cardId: 752,
     card: "A Dab Of Ink"
   },
   {
-    id: 764,
+    itemId: 764,
     name: "Tabula Rasa",
     cardId: 485,
     card: "Humility"
   }
 ]; 
 
-module.exports = items;
+module.exports = {itemCardTableEntries, cardIdToIndex};
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-let items = require('./items');
+let itemCardTableEntries = require('./items').itemCardTableEntries;
+let cardIdToIndex = require('./items').cardIdToIndex;
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const axios = require('axios');
@@ -19,58 +20,99 @@ app.use(limiter);
 let exchangeRate;
 
 app.get('/', (req, res) => {
+  if (Object.entries(cardIdToIndex).length === 0) {
+    buildItemIdList();
+  }
+  getItemData();
   res.send('hello world');
 });
 
-app.get('/table', cache.get, (req, res) => {
-  let itemPromises = [];
-  
-  axios.get('https://api.poe.watch/item?id=142')
-    .then(response => {
-      exchangeRate = Math.floor(response.data.data[0].mean);
+// https://api.poe.watch/compact?league=Synthesis gets us all the items and their prices
+// for each item in the response, see if the item id matches a cardId in our itemCardTableEntries
+// to do this - use the response item id to lookup in cardIdToIndex to find the index into our itemCardTableEntries
 
-      for (let item of items) {
+// build a dictionary of cardId to index into our itemCardTableEntries
+const buildItemIdList = () => {
+  for (let i = 0; i < itemCardTableEntries.length; i++) {
+    var entry = itemCardTableEntries[i];
+    cardIdToIndex[entry.cardId] = i;
+    cardIdToIndex[entry.itemId] = i;
+  }
+}
 
-         // 3x exalted orbs has the ID -1
-         // need only the card data for this
-         // can hardcode the itemPriceCh using exchangeRate and itemPriceEx
-         // 2x exalted orbs has the ID -2
-         // 3x annulment orbs has the ID -3
-         // need to do api call to get value of Orb Of Annulment
-         if (item.id < 0) break; 
-          itemPromises.push(
-            axios.get('https://api.poe.watch/item?id=' + item.id)
-              .then(response => getCardData(item, response.data))
-              .catch(err => console.log(err))
-          );
+const getItemData = () => {
+  axios.get("https://api.poe.watch/compact?league=Synthesis")
+    .then(res => {
+      for (let i = 0; i < res.data.length; i++) {
+        var lookupId = res.data[i].id;
+        var tableEntriesIndex = cardIdToIndex[lookupId];
+        if (tableEntriesIndex) {
+          var entry = itemCardTableEntries[tableEntriesIndex];
+
+          if (lookupId == entry.cardId) {
+            entry.cardPriceCh = res.data[i].median;
+          }
+          else {
+            entry.itemPriceCh = res.data[i].median;
+          }
+          
+        }
       }
-      Promise.all(itemPromises).then(items => {
-        cache.set(req, items);
-        res.send(items)
-      });
+      console.log(itemCardTableEntries);
     })
     .catch(err => console.log(err))
-});
 
-const getCardData = (item, itemData) => {
-  return axios.get('https://api.poe.watch/item?id=' + item.cardId)
-    .then(response => populateItemObject(item, itemData, response.data))
-    .catch(err => console.log(err));
 }
 
-const populateItemObject = (item, itemData, cardData) => {
-  item.stack = cardData.stack;
-  item.cardPriceCh = cardData.data[0].median;
-  item.cardPriceEx = cardData.data[0].median / exchangeRate;
-  item.stackPriceCh = cardData.stack * item.cardPriceCh;
-  item.stackPriceEx = cardData.stack * item.cardPriceEx;
-  item.itemPriceCh = itemData.data[0].median;
-  item.itemPriceEx = itemData.data[0].median / exchangeRate;
-  item.profitCh = item.itemPriceCh - item.stackPriceCh;
-  item.profitEx = item.itemPriceEx - item.stackPriceEx;
-  item.margin = 100 * item.profitCh / item.itemPriceCh;
-  return item;
-}
+// app.get('/table', cache.get, (req, res) => {
+//   let itemPromises = [];
+
+//   axios.get('https://api.poe.watch/item?id=142')
+//     .then(response => {
+//       exchangeRate = Math.floor(response.data.data[0].mean);
+
+//       for (let item of items) {
+
+//         // 3x exalted orbs has the ID -1
+//         // need only the card data for this
+//         // can hardcode the itemPriceCh using exchangeRate and itemPriceEx
+//         // 2x exalted orbs has the ID -2
+//         // 3x annulment orbs has the ID -3
+//         // need to do api call to get value of Orb Of Annulment
+//         if (item.id < 0) continue;
+//         itemPromises.push(
+//           axios.get('https://api.poe.watch/item?id=' + item.id)
+//             .then(response => getCardData(item, response.data))
+//             .catch(err => console.log(err))
+//         );
+//       }
+//       Promise.all(itemPromises).then(items => {
+//         cache.set(req, items);
+//         res.send(items)
+//       });
+//     })
+//     .catch(err => console.log(err))
+// });
+
+// const getCardData = (item, itemData) => {
+//   return axios.get('https://api.poe.watch/item?id=' + item.cardId)
+//     .then(response => populateItemObject(item, itemData, response.data))
+//     .catch(err => console.log(err));
+// }
+
+// const populateItemObject = (item, itemData, cardData) => {
+//   item.stack = cardData.stack;
+//   item.cardPriceCh = cardData.data[0].median;
+//   item.cardPriceEx = cardData.data[0].median / exchangeRate;
+//   item.stackPriceCh = cardData.stack * item.cardPriceCh;
+//   item.stackPriceEx = cardData.stack * item.cardPriceEx;
+//   item.itemPriceCh = itemData.data[0].median;
+//   item.itemPriceEx = itemData.data[0].median / exchangeRate;
+//   item.profitCh = item.itemPriceCh - item.stackPriceCh;
+//   item.profitEx = item.itemPriceEx - item.stackPriceEx;
+//   item.margin = 100 * item.profitCh / item.itemPriceCh;
+//   return item;
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
